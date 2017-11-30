3644111784fb4a147deb27913ed6180d68a5c960
Luiz <luizfelipebasile@gmail.com>
2017-11-30 00:28:08 -0200

    Testes

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
+use App\Models\Operation;
 class UserTest extends TestCase
 {
     /**
@@ -14,7 +15,18 @@ class UserTest extends TestCase
      */
     public function testExample()
     {
-        $user = new User([
+        $response = $this->get('/');
+        $this->assertTrue(true);
+       
+    }
+    public function testLogin()
+    {
+        $response = $this->get('/login');
+        $this->assertTrue(true);
+       
+    }
+    public function testAdminHome(){
+         $user = new User([
             'id' => '2',
             'name' => 'admin',
             'email' => 'admin@gmail.com',
@@ -22,7 +34,76 @@ class UserTest extends TestCase
             'is_permission' => '1',
         ]);
          $this->actingAs($user);
-    	$response = $this->get('/admin');
+        $response = $this->get('/admin');
         $this->assertTrue(true);
     }
+     public function testAdminImmobile(){
+         $user = new User([
+            'id' => '2',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+            'is_permission' => '1',
+        ]);
+         $this->actingAs($user);
+        $response = $this->get('/admin/immobile');
+        $this->assertTrue(true);
+    }
+    public function testAdminImmobileCreate(){
+         $user = new User([
+            'id' => '2',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+            'is_permission' => '1',
+        ]);
+         $this->actingAs($user);
+        $response = $this->get('/admin/immobile/create');
+        $this->assertTrue(true);
+    }
+    /*
+    public function testeController(){
+        $operation = new Operation([
+            'id' => '1',
+            'name' => 'asdasdf',
+        ]); 
+        $response = $this->action('GET', 'OperationsController@show', ['id' => 1]);
+    }
+     public function testAdminImmobileCreateParams(){
+         $user = new User([
+            'id' => '2',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+            'is_permission' => '1',
+        ]);
+         $this->actingAs($user);
+         $operation = new Operation([
+            'id' => '1',
+            'name' => 'asdasdf',
+        ]);  
+         echo($operation);
+         $response = $this->json('POST', '/admin/operation', ['id' => '1',
+                                                                    'name' => 'Sally',
+                                                                    'state' => 'Sally',
+                                                                    'city' => 'Sally',
+                                                                    'neighborhood' => 'Sally',
+                                                                    'number' => '2',
+                                                                    'type' => 'Sally',
+                                                                    'bedroom' => '3',
+                                                                    'area' => '500',
+                                                                    'value' => '500',
+                                                                    'description' => 'xxxx',
+                                                                    'operation_id' => '1',
+
+
+                                                                                        ]);
+         
+         $response
+            ->assertStatus(200)
+            ->assertJson([
+                'created' => true,
+            ]);
+    }*/
+     
 }
